007a5519a6ef468c5550d2864605930acfae6aa3
capaisa <capaisa12@gmail.com>
2018-06-11 13:43:24 -0500

    Se cambian las rutas por resources

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,48 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+def create
+  @post = Post.new(post_params)
+  if @post.save
+    redirect_to posts_path, notice: "El post ha sido publicado exitosamente!"
+  else
+    render :new
+  end
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to posts_path, notice: "El post ha sido actualizado satisfactoriamente!"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  post = Post.find(params[:id])
+  post.destroy
+  redirect_to posts_path, notice: "El producto ha sido eliminado de forma exitosa!"
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :content, :email)
+end
+
 end
