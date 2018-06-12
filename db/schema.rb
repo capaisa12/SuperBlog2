28391e5f9c5629617c5e1a50062aecc698cb2c8a
capaisa <capaisa12@gmail.com>
2018-06-11 23:29:18 -0500

    Implementa funcionalidad de registrar usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180609152937) do
+ActiveRecord::Schema.define(version: 20180612015958) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180609152937) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
