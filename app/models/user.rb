301293784906c76e734c92cc030247fa5352c016
capaisa <capaisa12@gmail.com>
2018-06-13 12:46:22 -0500

    Cambia el idioma a español

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@
 
 class User < ApplicationRecord
   has_secure_password validations: false
+
+  validates :email, uniqueness: true, format: /@/
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..20 }, allow_nil: true
 end
