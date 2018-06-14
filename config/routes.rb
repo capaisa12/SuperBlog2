08ba00c00fd24af24357511b125a2d34788b0075
capaisa <capaisa12@gmail.com>
2018-06-14 16:29:10 -0500

    Se agrega la funcionalidad de comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
